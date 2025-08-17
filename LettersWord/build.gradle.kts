834b9f9195b461080c65abdee0ad4db770fd4cfc
s-ball <s-ball@laposte.net>
2025-08-17 20:18:41 +0200

    Bump version to 0.7.2b

--- LettersWord/build.gradle.kts
+++ LettersWord/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 3
-        versionName = "0.7.1"
+        versionName = "0.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
